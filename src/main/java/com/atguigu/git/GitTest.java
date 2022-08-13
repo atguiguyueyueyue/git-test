39151e15fcbef07bac7b41feb548ec5ff2d05651
WangKun <1355379492@qq.com>
2022-08-13 16:49:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author WangKun
+ * @create 2022-08-13 16:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
